56237332ab6362213cd9193389e4c655676317e3
Edson Amaya <edson_amaya99@live.com>
2022-02-27 17:11:49 -0600

    11

---
+++ Xamarin/Xamarin/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Xamarin.Forms.PlatformConfiguration;
+using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
+
+namespace Xamarin
+{
+    public partial class App : Xamarin.Forms.Application
+    {
+        public App()
+        {
+            var navigationPage = new Xamarin.Forms.NavigationPage(new MainPage())
+            {
+                BarBackgroundColor = Color.FromHex("0099B4"),
+                BarTextColor = Color.White
+            };
+
+            navigationPage.On<iOS>().SetPrefersLargeTitles(true);
+
+            MainPage = navigationPage;
+        }
+    }
+}
---
+++ Xamarin/Xamarin/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Xamarin.ViewModels
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        DetailPageViewModel detailViewModel = new DetailPageViewModel();
+        NoteModel auxNote;
+        public MainPageViewModel()
+        {
+            Notes = new ObservableCollection<NoteModel>();
+
+            SaveNoteCommand = new Command(() =>
+            {
+                Notes.Add(new NoteModel { Text = NoteText });
+                NoteText = string.Empty;
+            },
+            () => !string.IsNullOrEmpty(NoteText));
+
+            EraseNotesCommand = new Command(() => Notes.Clear());
+
+            NoteSelectedCommand = new Command(async () =>
+            {
+                if (SelectedNote is null)
+                    return;
+
+                detailViewModel.NoteText =  SelectedNote.Text;
+                {
+                    auxNote = SelectedNote;
+                };
+
+                await Application.Current.MainPage.Navigation.PushAsync(new DetailPage(detailViewModel));
+
+                SelectedNote = null;
+            });
+
+            detailViewModel.EditCommand = new Command(async () =>
+            {
+                await Application.Current.MainPage.Navigation.PopAsync();
+
+                int posicion = Notes.IndexOf(auxNote);
+                if (posicion >= 0)
+                    Notes[posicion] = new NoteModel { Text = detailViewModel.NoteText };
+                string what = Notes[posicion].Text;
+            });
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        string noteText;
+        public string NoteText
+        {
+            get => noteText;
+            set
+            {
+                noteText = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(NoteText)));
+
+                SaveNoteCommand.ChangeCanExecute();
+            }
+        }
+
+        NoteModel selectedNote;
+        public NoteModel SelectedNote
+        {
+            get => selectedNote;
+            set
+            {
+                selectedNote = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(SelectedNote)));
+            }
+        }
+
+        public ObservableCollection<NoteModel> Notes { get; }
+
+        public Command NoteSelectedCommand { get; }
+        public Command SaveNoteCommand { get; }
+        public Command EraseNotesCommand { get; }
+    }
+}
